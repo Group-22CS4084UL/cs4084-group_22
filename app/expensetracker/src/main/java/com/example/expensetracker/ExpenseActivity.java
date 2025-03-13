cf2d0b8ee60a0d8cf9c9743a24f5640a2c318e71
elsawang <elsa.yilin.w39@gmail.com>
2025-03-13 12:01:30 +0000

    experiment

---
+++ app/expensetracker/src/main/java/com/example/expensetracker/ExpenseActivity.java
@@ -0,0 +1,6 @@
+package com.example.expensetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExpenseActivity extends AppCompatActivity {
+}
---
+++ app/expensetracker/src/main/java/com/example/expensetracker/SpendingVisualizationActivity.java
@@ -0,0 +1,6 @@
+package com.example.expensetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SpendingVisualizationActivity extends AppCompatActivity {
+}
